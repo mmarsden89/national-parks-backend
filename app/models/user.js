ec942e6173caed5a7f43feb32378e1a4af72ac0b
mmarsden89 <mmarsden89@gmail.com>
2020-10-15 00:27:53 -0700

    add default to user schema

--- app/models/user.js
+++ app/models/user.js
@@ -1,23 +1,29 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
-  nickname: {
-    type: String,
-    required: true,
-    unique: true
+const userSchema = new mongoose.Schema(
+  {
+    nickname: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    list: [
+      {
+        type: mongoose.Schema.Types.Mixed,
+        ref: "Park",
+        default: [],
+      },
+    ],
   },
-  list: [{
-    type: mongoose.Schema.Types.Mixed,
-    ref: 'Park'
-  }]
-}, {
-  timestamps: true,
-  toObject: {
-    // remove `hashedPassword` field when we call `.toObject`
-    transform: (_doc, user) => {
-      return user
-    }
+  {
+    timestamps: true,
+    toObject: {
+      // remove `hashedPassword` field when we call `.toObject`
+      transform: (_doc, user) => {
+        return user;
+      },
+    },
   }
-})
+);
 
-module.exports = mongoose.model('User', userSchema)
+module.exports = mongoose.model("User", userSchema);
